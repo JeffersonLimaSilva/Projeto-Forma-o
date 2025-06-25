7e60797b4ab2325a15492795f5a9f82596d91380
JeffersonLimaSilva <147891207+JeffersonLimaSilva@users.noreply.github.com>
2025-06-25 13:15:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let cadcor = document.querySelector('#cadastro-span')
+let homecor = document.querySelector('#home-span')
+let relcor = document.querySelector('#relatorio-span')
+
+cadcor.classList='span-off'
+homecor.classList='span-on'
+relcor.classList='span-off'
+
+let homeblack = document.querySelector('#home-black')
+let home = document.querySelector('#home')
+homeblack.className='image'
+home.className='image-off'
